65ce01e017289e9f43e6bbce20300b245c0fc2ee
Leon V <48109980+LeonVillanueva@users.noreply.github.com>
2019-09-25 21:23:28 -0400

    Update : Udacity : predict.py file

--- Udacity/SageMaker incl NN Sentiment Analysis/Project/serve/predict.py
+++ Udacity/SageMaker incl NN Sentiment Analysis/Project/serve/predict.py
@@ -72,46 +72,37 @@ def predict_fn(input_data, model):
 
     data_X = None
     data_len = None
+
+    # Using data_X and data_len we construct an appropriate input tensor. Remember
+    # that our model expects input data of the form 'len, review[500]'.
+    data_pack = np.hstack((data_len, data_X))
+    data_pack = data_pack.reshape(1, -1)
     
-    #-----@-----#
-    
+    data = torch.from_numpy(data_pack)
+    data = data.to(device)
     input_data_words = review_to_words(input_data)
     data_X, data_len = convert_and_pad(model.word_dict, input_data_words)
-    # data_X = pd.concat([pd.DataFrame(test_data_len), pd.DataFrame(test_data)], axis=1)
-
-    # Using data_X and data_len we construct an appropriate input tensor. Remember
-    # that our model expects input data of the form 'len, review[500]'.
-    # data_pack = np.hstack((data_len, data_X))
-    data_pack = np.hstack((data_len, data_X))
     
-    data_pack = data_pack.reshape(1, -1)
-    
-    data = torch.from_numpy(data_pack)
-    data = data.to(device)
-
-    # Make sure to put the model into evaluation mode
-    model.eval()
-    # Using data_X and data_len we construct an appropriate input tensor. Remember
-    # that our model expects input data of the form 'len, review[500]'.
     data_pack = np.hstack((data_len, data_X))
     data_pack = data_pack.reshape(1, -1)
     
     data = torch.from_numpy(data_pack)
     data = data.to(device)
 
+    
     # Make sure to put the model into evaluation mode
     model.eval()
 
     # TODO: Compute the result of applying the model to the input data. The variable `result` should
     #       be a numpy array which contains a single integer which is either 1 or 0
-
-    result = None
-    
     with torch.no_grad():
         output = model.forward(data)
         
     result = np.round(output.numpy())
+    # result = predictor.predict(data.values)
+    print(result)
 
     return result
-    
+    result = None
+
     return result
