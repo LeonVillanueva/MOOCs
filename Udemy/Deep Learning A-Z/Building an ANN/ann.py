9772e5be2963d12436f5186dc50d5b5d3e743521
Leon V <48109980+LeonVillanueva@users.noreply.github.com>
2019-10-02 20:48:19 -0400

    Update : Udemy : Preprocessing updated
    Summary : Labeler + OneHot encoder has been deprecated in favor of ColumnTransformer preprocessing

--- Udemy/Deep Learning A-Z/Building an ANN/ann.py
+++ Udemy/Deep Learning A-Z/Building an ANN/ann.py
@@ -20,8 +20,11 @@ import pandas as pd
 dataset = pd.read_csv('Churn_Modelling.csv')
 X = dataset.iloc[:, 3:13].values
 y = dataset.iloc[:, 13].values
-
+Xc = X.copy()
+Xo = X.copy()
 # Encoding categorical data
+
+
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 labelencoder_X_1 = LabelEncoder()
 X[:, 1] = labelencoder_X_1.fit_transform(X[:, 1])
@@ -30,6 +33,29 @@ X[:, 2] = labelencoder_X_2.fit_transform(X[:, 2])
 onehotencoder = OneHotEncoder(categorical_features = [1])
 X = onehotencoder.fit_transform(X).toarray()
 X = X[:, 1:]
+
+
+    # will be deprecated @ 2.2
+    # use column transformer insted
+    # https://jorisvandenbossche.github.io/blog/2018/05/28/scikit-learn-columntransformer/
+    # https://scikit-learn.org/stable/auto_examples/compose/plot_column_transformer_mixed_types.html
+    
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+
+categorical_features = [1, 2]
+numerical_features = list (range(3,11))
+categorical_transformer = Pipeline(steps=[('onehot', OneHotEncoder(handle_unknown='ignore'))])
+
+preprocess = ColumnTransformer(transformers=[
+        ('cat', categorical_transformer, categorical_features)])
+
+    # DeprecationWarning: `make_column_transformer` now expects (transformer, columns) as input tuples instead of (columns, transformer).
+    # https://towardsdatascience.com/preprocessing-with-sklearn-a-complete-and-comprehensive-guide-670cb98fcfb9
+
+Xp = None
+Xp = preprocess.fit_transform(Xc)
 
 # Splitting the dataset into the Training set and Test set
 from sklearn.model_selection import train_test_split
@@ -62,7 +88,7 @@ classifier.add(Dense(units = 1, kernel_initializer = 'uniform', activation = 'si
 
 # Compiling the ANN
 classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
-
+    # https://towardsdatascience.com/advanced-keras-constructing-complex-custom-losses-and-metrics-c07ca130a618
 # Fitting the ANN to the Training set
 classifier.fit(X_train, y_train, batch_size = 10, epochs = 100)
 
