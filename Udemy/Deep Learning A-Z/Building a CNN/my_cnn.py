0ecd3eaec16b2118f4f69de94c1f6f48948dfb18
Leon V <48109980+LeonVillanueva@users.noreply.github.com>
2019-10-09 18:40:12 -0400

    Course : Udemy : CNN Build Initial
    Summary : Initial commit of CNN building project with Keras

---
+++ Udemy/Deep Learning A-Z/Building a CNN/my_cnn.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct  9 18:09:02 2019
+
+@author: FartherSkies
+"""
+
+from keras.models import Sequential
+from keras.layers import Convolution2D
+from keras.layers import MaxPooling2D
+from keras.layers import Flatten
+from keras.layers import Dense
+
