54df6c785374c1c026803466b6a7fbb0580bce22
Leon V <48109980+LeonVillanueva@users.noreply.github.com>
2019-10-13 08:15:42 -0400

    Update : Udemy : model file save, predictions
    Results : accuracy: 0.9641 - val_loss: 0.9642 - val_accuracy: 0.7683

--- Udemy/Deep Learning A-Z/Building a CNN/my_cnn.py
+++ Udemy/Deep Learning A-Z/Building a CNN/my_cnn.py
@@ -54,6 +54,24 @@ test_generator = test_datagen.flow_from_directory(
 classifier.fit_generator(
         train_generator,
         steps_per_epoch=6400,
-        epochs=32,
+        epochs=8,
         validation_data=test_generator,
         validation_steps=1600)
+
+classifier.save('dogcat.h5')
+
+import numpy as np
+from keras.preprocessing import image
+
+test_image = image.load_img('dataset/single_prediction/cat_or_dog_2.jpg', target_size = (64, 64))
+test_image = image.img_to_array(test_image)
+test_image = np.expand_dims(test_image, axis = 0)
+result = classifier.predict(test_image)
+train_generator.class_indices
+
+if result[0][0] == 1:
+    prediction = 'dog'
+else:
+    prediction = 'cat'
+
+print (prediction)
