d5622e79b02656d20aa6a7eb015aa5a4ca55c237
Leon V <48109980+LeonVillanueva@users.noreply.github.com>
2019-10-14 20:26:19 -0400

    Update : Udemy : train/test gen, scaling

--- Udemy/Deep Learning A-Z/Building an RNN/my_rnn.py
+++ Udemy/Deep Learning A-Z/Building an RNN/my_rnn.py
@@ -12,4 +12,23 @@ import pandas as pd
 dataset_train = pd.read_csv('Google_Stock_Price_Train.csv')
 training_set = dataset_train.iloc[:, 1:2].values
 
-print (training_set[:10])
+# print (training_set[:10])
+
+# scaling
+# normalization = sigmoid
+# standardization = negatives, regression
+
+from sklearn.preprocessing import MinMaxScaler
+sc = MinMaxScaler (feature_range=(0,1))
+training_set_norm = sc.fit_transform(training_set)
+
+# observation of 60 day lag
+
+X_train = []
+y_train = []
+
+print (len(training_set_norm))
+
+for i in range (60, len(training_set_norm)):
+    X_train.append (training_set_norm[i-60:i,0])
+    y_train.append (training_set_norm[i, 0])
