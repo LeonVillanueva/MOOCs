479a1dab0b2c463ae3890d91a67b0300642d1dce
Leon V <48109980+LeonVillanueva@users.noreply.github.com>
2019-10-20 12:26:43 -0400

    Update : Udemy : SOM data preprocessing

--- Udemy/Deep Learning A-Z/Building my SOM/my_som.py
+++ Udemy/Deep Learning A-Z/Building my SOM/my_som.py
@@ -9,3 +9,16 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
+dataset = pd.read_csv ('Credit_Card_Applications.csv')
+
+# mean interneuron distance
+# euclidean distance between neuron - within a (defined) neighborhood
+
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
+
+# feature scaling
+
+from sklearn.preprocessing import MinMaxScaler
+sc = MinMaxScaler(feature_range = (0, 1))
+X_norm = sc.fit_transform (X)
