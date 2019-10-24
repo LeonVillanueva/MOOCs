e5cb9945cc6a2d297e94e337b2d005032d393895
Leon V <48109980+LeonVillanueva@users.noreply.github.com>
2019-10-24 13:02:31 -0400

    Update : Udemy : data parse function

--- Udemy/Deep Learning A-Z/Building my Boltzmann/my_rbm.py
+++ Udemy/Deep Learning A-Z/Building my Boltzmann/my_rbm.py
@@ -24,3 +24,17 @@ training_set = pd.read_csv ('ml-100k/u1.base', delimiter='\t')
 training_set = np.array (training_set, dtype='int')
 test_set = pd.read_csv ('ml-100k/u1.test', delimiter='\t')
 test_set = np.array (test_set, dtype='int')
+
+# of users and movies
+
+nb_movies = int (max (max (training_set[:,1]), max(test_set[:,1])))
+nb_users = int (max (max (training_set[:,0]), max(test_set[:,0])))
+
+def convert (data):
+	# output : list of list, all movies, 0 = not rated
+	new_data = []
+	# get all the movies and ratings in the training set per user
+	for id_users in range (1, nb_users+1):
+		id_movies = data[:,1][data[:,0]==id_users]
+		id_ratings = data[:,2][data[:,0]==id_users]
+		
