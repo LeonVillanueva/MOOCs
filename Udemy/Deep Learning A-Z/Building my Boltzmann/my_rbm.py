bb7291b14d7a7cf121bbbdf616244322cb09d43b
Leon V <48109980+LeonVillanueva@users.noreply.github.com>
2019-10-24 05:40:10 -0400

    Update : Udemy : loading data

--- Udemy/Deep Learning A-Z/Building my Boltzmann/my_rbm.py
+++ Udemy/Deep Learning A-Z/Building my Boltzmann/my_rbm.py
@@ -7,3 +7,15 @@ Created on Wed Oct 23 19:43:58 2019
 
 # boltzmann machine
 
+import numpy as np
+import pandas as pd
+import torch
+import torch.nn as nn
+import torch.nn.parallel
+import torch.optim as optim
+import torch.utils.data
+from torch.autograd import Variable
+
+movies = pd.read_csv ('ml-1m/movies.dat', sep='::', header=None, engine='python', encoding='latin-1')
+users = pd.read_csv ('ml-1m/users.dat', sep='::', header=None, engine='python', encoding='latin-1')
+movies = pd.read_csv ('ml-1m/movies.dat', sep='::', header=None, engine='python', encoding='latin-1')
