23963f434481191150d3404c7d82c77c354420e4
Leon V <48109980+LeonVillanueva@users.noreply.github.com>
2019-10-31 19:06:09 -0400

    Update : Udemy : re-initialize my_ae.py

--- Udemy/Deep Learning A-Z/Building my AutoEncoder/my_ae.py
+++ Udemy/Deep Learning A-Z/Building my AutoEncoder/my_ae.py
@@ -5,14 +5,11 @@ Created on Wed Oct 30 11:35:11 2019
 @author: FartherSkies
 """
 
-# Data Preprocessing
-
-# Importing the libraries
 import numpy as np
-import matplotlib.pyplot as plt
 import pandas as pd
-
-# Importing the dataset
-dataset = pd.read_csv('Data.csv')
-X = dataset.iloc[:, :-1].values
-y = dataset.iloc[:, 3].values
+import torch
+import torch.nn as nn
+import torch.nn.parallel
+import torch.optim as optim
+import torch.utils.data
+from torch.autograd import Variable
