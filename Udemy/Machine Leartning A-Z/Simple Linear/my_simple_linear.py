f8cd0ef7533af582a0c353ca490c7053b41946ad
Leon V <48109980+LeonVillanueva@users.noreply.github.com>
2019-11-11 06:16:32 -0500

    Update : Udemy : split, train, predict

--- Udemy/Machine Leartning A-Z/Simple Linear/my_simple_linear.py
+++ Udemy/Machine Leartning A-Z/Simple Linear/my_simple_linear.py
@@ -12,10 +12,22 @@ import pandas as pd
 
 # Importing the dataset
 dataset = pd.read_csv('Salary_Data.csv')
-X = dataset.iloc[:, 0].values
+X = dataset.iloc[:, :-1].values # preserve the 2D array
 y = dataset.iloc[:, 1].values
 
 # Splitting the dataset into the Training set and Test set
 from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 1/3, random_state = 0)
+
+
+from sklearn.linear_model import LinearRegression
+lr = LinearRegression ()
+
+lr.fit (X_train, y_train)
+y_pred = lr.predict (X_test)
+
+lr.coef_
+lr.intercept_
+
+import matplotlib.pyplot as plt
 
