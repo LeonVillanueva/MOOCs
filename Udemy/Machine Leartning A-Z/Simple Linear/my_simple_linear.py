fa0e2a98538027be7a5ced0aecf43eb884225bfb
Leon V <48109980+LeonVillanueva@users.noreply.github.com>
2019-11-11 11:29:05 -0500

    Update : Udemy : graphical, metrics

--- Udemy/Machine Leartning A-Z/Simple Linear/my_simple_linear.py
+++ Udemy/Machine Leartning A-Z/Simple Linear/my_simple_linear.py
@@ -21,13 +21,28 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 1/3, rando
 
 
 from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score
+
 lr = LinearRegression ()
 
 lr.fit (X_train, y_train)
 y_pred = lr.predict (X_test)
+r2 = r2_score(y_true, y_pred)
 
-lr.coef_
-lr.intercept_
+print ('r-squared: '+r2)
+print ('coefficent of age: '+lr.coef_)
+print ('starting salary: '+lr.intercept_)
 
-import matplotlib.pyplot as plt
+loss = np.sqrt ((y_test - y_pred)**2)
+
+# graphical
+
+plt.scatter (X_test, y_test, color='red')
+plt.plot (X_test, y_pred, color='blue')
+plt.plot (X_test, loss, color='red', alpha=0.30)
+plt.title ('Years v Salary')
+plt.xlabel ('Years of Experience')
+plt.ylabel ('Salary')
+plt.show()
+
 
