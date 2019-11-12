d15ce9fb0161a5a5ae2be7cf237bb39a7c1155b6
Leon V <48109980+LeonVillanueva@users.noreply.github.com>
2019-11-12 05:19:43 -0500

    Course : Udemy : multilinear setup initial

---
+++ Udemy/Machine Leartning A-Z/Multilinear/my_multilinear.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 12 05:14:58 2019
+
+@author: FartherSkies
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('50_Startups.csv')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, 4].values
+
+# Encoding categorical data
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelencoder = LabelEncoder()
+X[:, 3] = labelencoder.fit_transform(X[:, 3])
+onehotencoder = OneHotEncoder(categorical_features = [3])
+X = onehotencoder.fit_transform(X).toarray()
