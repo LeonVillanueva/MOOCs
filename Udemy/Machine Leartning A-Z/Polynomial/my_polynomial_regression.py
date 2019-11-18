1d64b4024ae7f282b34c1e01de5962d5711d8e87
Leon V <48109980+LeonVillanueva@users.noreply.github.com>
2019-11-18 18:24:51 -0500

    Update : Udemy : polynomial fit/train

--- Udemy/Machine Leartning A-Z/Polynomial/my_polynomial_regression.py
+++ Udemy/Machine Leartning A-Z/Polynomial/my_polynomial_regression.py
@@ -31,3 +31,10 @@ X_test = sc_X.transform(X_test)"""
 from sklearn.linear_model import LinearRegression
 lin_reg = LinearRegression()
 lin_reg.fit(X, y)
+
+from sklearn.preprocessing import PolynomialFeatures
+poly_reg = PolynomialFeatures (degree=2)
+X_poly = poly_reg.fit_transform (X)
+
+lin_reg_2 = LinearRegression()
+lin_reg_2.fit(X_poly, y)
