e6d9f488ab0a7bd6b92f3f37dd2657f88995ec0e
Leon V <48109980+LeonVillanueva@users.noreply.github.com>
2019-12-21 22:47:17 -0500

    Course : Udemy : logistic regression initialize

---
+++ Udemy/Machine Leartning A-Z/Logistic Regression/my_logistic_regression.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec 21 20:07:52 2019
+
+@author: FartherSkies
+"""
+
+# Logistic Regression
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Social_Network_Ads.csv')
+X = dataset.iloc[:, [2, 3]].values
+y = dataset.iloc[:, 4].values
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
