aa655b263a844c44b789621e9d1b7935263946c9
Leon V <48109980+LeonVillanueva@users.noreply.github.com>
2019-12-22 13:32:48 -0500

    Update : Udemy : logit scale, fit_predict
    Summary : Logistic regression, standard scaling, prediction, plotting

--- Udemy/Machine Leartning A-Z/Logistic Regression/my_logistic_regression.py
+++ Udemy/Machine Leartning A-Z/Logistic Regression/my_logistic_regression.py
@@ -20,3 +20,42 @@ y = dataset.iloc[:, 4].values
 # Splitting the dataset into the Training set and Test set
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
+
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler ()
+X_train = sc_X.fit_transform (X_train)
+X_test = sc_X.fit_transform (X_test)
+
+from sklearn.linear_model import LogisticRegression
+class_logit = LogisticRegression (random_state = 0)
+class_logit.fit (X_train, y_train)
+
+y_pred = class_logit.predict (X_test)
+y_pred_prob = class_logit.predict_proba (X_test)
+print (y_pred)
+print (y_pred_prob)
+
+# Confusion Matrix
+
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix (y_test, y_pred)
+print (cm)
+
+# Plot
+
+from matplotlib.colors import ListedColormap
+X_set, y_set = X_test, y_test
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
+                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+plt.contourf(X1, X2, class_logit.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+                c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('Logistic Regression (Test set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()
