32a0ea576cc2ce331943224c54c2f7492caa4497
Leon V <48109980+LeonVillanueva@users.noreply.github.com>
2019-12-22 19:54:03 -0500

    Update : Udemy : knn application, graph
    Note : completed using the Logistic Regression classifier template

--- Udemy/Machine Leartning A-Z/K Nearest Neighbors/my_knn.py
+++ Udemy/Machine Leartning A-Z/K Nearest Neighbors/my_knn.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sun Dec 22 19:26:30 2019
+Created on Sat Dec 21 20:07:52 2019
 
 @author: FartherSkies
 """
 
-# K-Nearest Neighbors (K-NN)
+# K-NN
 
 # Importing the libraries
 import numpy as np
@@ -21,8 +21,42 @@ y = dataset.iloc[:, 4].values
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
 
-# Feature Scaling
 from sklearn.preprocessing import StandardScaler
-sc = StandardScaler()
-X_train = sc.fit_transform(X_train)
-X_test = sc.transform(X_test)
+sc_X = StandardScaler ()
+X_train = sc_X.fit_transform (X_train)
+X_test = sc_X.fit_transform (X_test)
+
+from sklearn.neighbors import KNeighborsClassifier
+class_knn = KNeighborsClassifier (n_neighbors=5, metric='minkowski', p=2)
+class_knn.fit (X_train, y_train)
+
+y_pred = class_knn.predict (X_test)
+y_pred_prob = class_knn.predict_proba (X_test)
+print (y_pred)
+print (y_pred_prob)
+
+# Confusion Matrix
+
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix (y_test, y_pred)
+print (cm)
+
+# Plot
+
+from matplotlib.colors import ListedColormap
+X_set, y_set = X_test, y_test
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.005),
+                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.005))
+plt.contourf(X1, X2, class_knn.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+                c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('KNN (Test set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()
+
