803d729a2e237e6f95910d8d0daea401878e532e
Leon V <48109980+LeonVillanueva@users.noreply.github.com>
2019-12-30 09:47:18 -0500

    Course : Udemy : thompson sampling init
    Note : thompson sampling = thompson

---
+++ Udemy/Machine Leartning A-Z/Thompson Sampling/my_thompson_sampling.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 30 09:44:31 2019
+
+@author: FartherSkies
+"""
+
+# Thompson Sampling
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Ads_CTR_Optimisation.csv')
+
+# Implementing Thompson Sampling
+import random
+N = 10000
+d = 10
+ads_selected = []
+numbers_of_rewards_1 = [0] * d
+numbers_of_rewards_0 = [0] * d
+total_reward = 0
