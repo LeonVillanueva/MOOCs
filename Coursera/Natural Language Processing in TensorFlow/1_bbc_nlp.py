d6a7000a9c4c476d2721393c2b277eafd19d859d
Leon V <48109980+LeonVillanueva@users.noreply.github.com>
2020-06-05 11:16:11 -0400

    Summary : Week 1, just notes no code

---
+++ Coursera/Natural Language Processing in TensorFlow/1_bbc_nlp.py
@@ -0,0 +1,65 @@
+# https://colab.research.google.com/github/lmoroney/dlaicourse/blob/master/TensorFlow%20In%20Practice/Course%203%20-%20NLP/Course%203%20-%20Week%201%20-%20Lesson%201.ipynb
+
+#https://colab.research.google.com/github/lmoroney/dlaicourse/blob/master/TensorFlow%20In%20Practice/Course%203%20-%20NLP/Course%203%20-%20Week%201%20-%20Lesson%202.ipynb#scrollTo=ArOPfBwyZtln
+
+# Sarcasm Dataset : https://rishabhmisra.github.io/publications/
+
+# including JSON : https://colab.research.google.com/github/lmoroney/dlaicourse/blob/master/TensorFlow%20In%20Practice/Course%203%20-%20NLP/Course%203%20-%20Week%201%20-%20Lesson%203.ipynb
+
+
+'''
+from tensorflow.keras.preprocessing.text import Tokenizer
+
+sentences = [
+    'i love my dog',
+    'I, love my cat',
+    'You love my dog!'
+]
+
+tokenizer = Tokenizer(num_words = 100)
+tokenizer.fit_on_texts(sentences)
+word_index = tokenizer.word_index
+print(word_index)
+
+
+import tensorflow as tf
+from tensorflow import keras
+
+
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+
+sentences = [
+    'I love my dog',
+    'I love my cat',
+    'You love my dog!',
+    'Do you think my dog is amazing?'
+]
+
+tokenizer = Tokenizer(num_words = 100, oov_token="<OOV>")
+tokenizer.fit_on_texts(sentences)
+word_index = tokenizer.word_index
+
+sequences = tokenizer.texts_to_sequences(sentences)
+
+padded = pad_sequences(sequences, maxlen=5)
+print("\nWord Index = " , word_index)
+print("\nSequences = " , sequences)
+print("\nPadded Sequences:")
+print(padded)
+
+
+# Try with words that the tokenizer wasn't fit to
+test_data = [
+    'i really love my dog',
+    'my dog loves my manatee'
+]
+
+test_seq = tokenizer.texts_to_sequences(test_data)
+print("\nTest Sequence = ", test_seq)
+
+padded = pad_sequences(test_seq, maxlen=10)
+print("\nPadded Test Sequence: ")
+print(padded)
+'''
+# https://github.com/Yoast/YoastSEO.js/blob/develop/src/config/stopwords.js
