1a56e65bf668acab1f87e8684a35bb4759d2a5d2
Leon V <48109980+LeonVillanueva@users.noreply.github.com>
2020-06-06 10:58:13 -0400

    Summary : Week 2, notes only no code

---
+++ Coursera/Natural Language Processing in TensorFlow/2_imdb_nlp.py
@@ -0,0 +1,13 @@
+# IMDB dataset : https://ai.stanford.edu/~amaas/data/sentiment/, https://github.com/tensorflow/datasets/blob/master/docs/catalog/imdb_reviews.md
+# notebook1 : https://colab.research.google.com/github/lmoroney/dlaicourse/blob/master/TensorFlow%20In%20Practice/Course%203%20-%20NLP/Course%203%20-%20Week%202%20-%20Lesson%201.ipynb
+
+# subtokens
+
+# notebook2 : https://colab.research.google.com/github/lmoroney/dlaicourse/blob/master/TensorFlow%20In%20Practice/Course%203%20-%20NLP/Course%203%20-%20Week%202%20-%20Lesson%202.ipynb
+
+# github datasets NLP : https://github.com/tensorflow/datasets/tree/master/docs/catalog
+
+# subwords text encoder : https://www.tensorflow.org/datasets/api_docs/python/tfds/features/text/SubwordTextEncoder
+
+# notebook3 : https://colab.research.google.com/github/lmoroney/dlaicourse/blob/master/TensorFlow%20In%20Practice/Course%203%20-%20NLP/Course%203%20-%20Week%202%20-%20Lesson%203.ipynb
+
