2e0234e1c2211034dd4af1d97166b424e587bed2
Leon V <48109980+LeonVillanueva@users.noreply.github.com>
2020-06-09 11:18:26 -0400

    Summary : Week 3, notes only, no code

---
+++ Coursera/Natural Language Processing in TensorFlow/3_nlp_lstm.py
@@ -0,0 +1,32 @@
+# RNN : https://www.coursera.org/lecture/nlp-sequence-models/deep-rnns-ehs0S
+
+# LSTM : https://www.coursera.org/lecture/nlp-sequence-models/long-short-term-memory-lstm-KXoay
+# single layer : https://colab.research.google.com/github/lmoroney/dlaicourse/blob/master/TensorFlow%20In%20Practice/Course%203%20-%20NLP/Course%203%20-%20Week%203%20-%20Lesson%201a.ipynb
+# multi layer : https://colab.research.google.com/github/lmoroney/dlaicourse/blob/master/TensorFlow%20In%20Practice/Course%203%20-%20NLP/Course%203%20-%20Week%203%20-%20Lesson%201b.ipynb
+
+'''
+BUFFER_SIZE = 10000
+BATCH_SIZE = 64
+
+train_dataset = train_dataset.shuffle(BUFFER_SIZE)
+train_dataset = train_dataset.padded_batch(BATCH_SIZE, train_dataset.output_shapes)
+test_dataset = test_dataset.padded_batch(BATCH_SIZE, test_dataset.output_shapes)
+
+model = tf.keras.Sequential([
+    tf.keras.layers.Embedding(tokenizer.vocab_size, 64),
+    tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(64, return_sequences=True)),
+    tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(32)),
+    tf.keras.layers.Dense(64, activation='relu'),
+    tf.keras.layers.Dense(1, activation='sigmoid')
+])
+'''
+
+# CONV1D + GLOBALMAXPOOLING (or GLOBALAVEPOOLINGNG) = FLATTEN?
+
+# convolutional with pooling : https://colab.research.google.com/github/lmoroney/dlaicourse/blob/master/TensorFlow%20In%20Practice/Course%203%20-%20NLP/Course%203%20-%20Week%203%20-%20Lesson%201c.ipynb
+
+# GRU : https://colab.research.google.com/github/lmoroney/dlaicourse/blob/master/TensorFlow%20In%20Practice/Course%203%20-%20NLP/Course%203%20-%20Week%203%20-%20Lesson%202d.ipynb#scrollTo=nHGYuU4jPYaj
+
+# bidirectional LSTM : https://colab.research.google.com/github/lmoroney/dlaicourse/blob/master/TensorFlow%20In%20Practice/Course%203%20-%20NLP/Course%203%20-%20Week%203%20-%20Lesson%202.ipynb#scrollTo=g9DC6dmLF8DC
+
+# 1d LSTM : https://colab.research.google.com/github/lmoroney/dlaicourse/blob/master/TensorFlow%20In%20Practice/Course%203%20-%20NLP/Course%203%20-%20Week%203%20-%20Lesson%202c.ipynb#scrollTo=g9DC6dmLF8DC
