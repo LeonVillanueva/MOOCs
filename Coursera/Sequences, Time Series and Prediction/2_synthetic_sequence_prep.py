1cfe40b318f52a23462216d58c77107864ab4bc4
Leon V <48109980+LeonVillanueva@users.noreply.github.com>
2020-06-24 14:24:04 -0400

    Summary : Week 2 notes, sample windowing

---
+++ Coursera/Sequences, Time Series and Prediction/2_synthetic_sequence_prep.py
@@ -0,0 +1,46 @@
+# windowing
+
+# notebook1 : https://colab.research.google.com/github/lmoroney/dlaicourse/blob/master/TensorFlow%20In%20Practice/Course%204%20-%20S%2BP/S%2BP%20Week%202%20Lesson%201.ipynb#scrollTo=DryEZ2Mz2nNV
+
+
+dataset = tf.data.Dataset.range(10)
+dataset = dataset.window(5, shift=1, drop_remainder=True)
+dataset = dataset.flat_map(lambda window: window.batch(5))
+dataset = dataset.map(lambda window: (window[:-1], window[-1:]))
+for x,y in dataset:
+  print(x.numpy(), y.numpy())
+
+# OR
+
+dataset = tf.data.Dataset.range(10)
+dataset = dataset.window(5, shift=1, drop_remainder=True)
+dataset = dataset.flat_map(lambda window: window.batch(5))
+dataset = dataset.map(lambda window: (window[:-1], window[-1:]))
+dataset = dataset.shuffle(buffer_size=10)
+dataset = dataset.batch(2).prefetch(1)
+for x,y in dataset:
+  print("x = ", x.numpy())
+  print("y = ", y.numpy())
+  
+# notebook2 : effectively linear regression : https://colab.research.google.com/github/lmoroney/dlaicourse/blob/master/TensorFlow%20In%20Practice/Course%204%20-%20S%2BP/S%2BP%20Week%202%20Lesson%202.ipynb
+
+# notebook3 : https://colab.research.google.com/github/lmoroney/dlaicourse/blob/master/TensorFlow%20In%20Practice/Course%204%20-%20S%2BP/S%2BP%20Week%202%20Lesson%203.ipynb
+
+''' sample learning rate scheduler
+
+dataset = windowed_dataset(x_train, window_size, batch_size, shuffle_buffer_size)
+
+
+model = tf.keras.models.Sequential([
+    tf.keras.layers.Dense(10, input_shape=[window_size], activation="relu"), 
+    tf.keras.layers.Dense(10, activation="relu"), 
+    tf.keras.layers.Dense(1)
+])
+
+lr_schedule = tf.keras.callbacks.LearningRateScheduler(
+    lambda epoch: 1e-8 * 10**(epoch / 20))
+optimizer = tf.keras.optimizers.SGD(lr=1e-8, momentum=0.9)
+model.compile(loss="mse", optimizer=optimizer)
+history = model.fit(dataset, epochs=100, callbacks=[lr_schedule], verbose=0)
+'''
+
