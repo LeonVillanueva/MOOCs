1ac75d570dd0b03652398028b35fe13d658b55d6
Leon V <48109980+LeonVillanueva@users.noreply.github.com>
2020-08-11 18:04:31 -0400

    Summary : further functions

--- Coursera/Natural Language Processing with Probabilistic Model/1_autocorrect.py
+++ Coursera/Natural Language Processing with Probabilistic Model/1_autocorrect.py
@@ -56,3 +56,49 @@ def delete_letter(word):
         delete_l.append (''.join(chars))
 
     return delete_l
+
+def switch_letter(word, verbose=False):
+    switch_l = []
+    split_l = []
+
+    for char in range(len(word)):
+        L = ''.join(word[0:char])
+        R = ''.join(word[char:])
+        split_l.append ((L, R))
+
+    chars = [char for char in word]
+    length = len(chars)
+    if length >= 2:
+        for i in range(length-1):
+            first = i
+            second = i+1
+            switch_l.append(''.join(chars[:first])+chars[second]+chars[first]+''.join(chars[second+1:]))
+
+    if verbose: print(f"Input word = {word} \nsplit_l = {split_l} \nswitch_l = {switch_l}")
+    return switch_l
+
+def replace_letter(word, verbose=False):
+
+    letters = 'abcdefghijklmnopqrstuvwxyz'
+    replace_l = []
+    split_l = []
+
+    for char in range(len(word)):
+        L = ''.join(word[0:char])
+        R = ''.join(word[char:])
+        split_l.append ((L, R))
+
+    for i in range (len(word)):
+        chars = [char for char in word]
+        for j in range (len(letters)):
+            chars[i] = [z for z in letters][j]
+            replace_l.append (''.join(chars))
+
+    replace_l = filter(lambda i: i not in word, replace_l)
+    replace_set = set (replace_l)
+
+    replace_l = sorted(list(replace_set))
+
+    if verbose: print(f"Input word = {word} \nsplit_l = {split_l} \nreplace_l {replace_l}")
+
+    return replace_l
