8b5a17b98edac4e58e273ccb273ae3cd59a341a0
Leon V <48109980+LeonVillanueva@users.noreply.github.com>
2020-09-02 18:51:19 -0400

    Summary : word embeddings, ngram model ipynb

---
+++ Coursera/Natural Language Processing with Probabilistic Model/4_word_embeddings.py
@@ -0,0 +1,16 @@
+# word embeddings
+'''
+word vector trainings
+    1. word representations
+    2. generate word embeddings
+    3. prepare text for machine learning
+    4. implement Bag of Words
+
+basic word representations
+    1. integers
+    2. one-hot vectors
+    3. word embeddings
+
+word embeddings
+
+'''
